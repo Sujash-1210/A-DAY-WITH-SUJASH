167bd9d444271dee8077325a5b5cfc1f3e1d40de
Sujash-1210 <sujashdas80@gmail.com>
2025-07-07 13:36:56 +0530

    update bugs

--- Backend/index.js
+++ Backend/index.js
@@ -6,23 +6,13 @@ import adminVideoRoutes from './routes/admin/adminVideoRoutes.js'
 import userVideoRoutes from './routes/user/userVideoRoutes.js'
 import adminAuthRoutes from './routes/admin/adminAuthRoutes.js'
 import userContactRoutes from './routes/user/userContactRoutes.js'
-import path from 'path'
-import { fileURLToPath } from 'url';
-
-// Get the current directory of the file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-//const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-// Set up the uploads directory path
-const uploadsDir = path.join(__dirname, 'uploads');
 
 dotenv.config()
 const app = express()
 app.use(cors())
 await connectDB()
 app.use(express.json())
-app.use('/uploads', express.static(uploadsDir));
+app.use('/uploads', express.static("uploads"));
 
 app.use('/admin',adminVideoRoutes)
 app.use('/admin',adminAuthRoutes)
--- Backend/middlewares/upload.js
+++ Backend/middlewares/upload.js
@@ -1,23 +1,10 @@
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
-
-// Create 'uploads' folder if it doesn't exist
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-    fs.mkdirSync(uploadsDir);
-}
-
+import multer from 'multer'
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        // Ensure that the destination is the 'uploads' directory
-        cb(null, uploadsDir);
-    },
-    filename: (req, file, cb) => {
-        // Create a unique file name with current timestamp and original file name
-        const uniqueFileName = `${Date.now()}-${file.originalname}`;
-        cb(null, uniqueFileName);
+    destination: 'uploads/',
+    filename:(req,file,cb)=>{
+        const uniqueFileName = `${Date.now()}${file.originalname}`
+        cb(null,uniqueFileName)
     }
-});
+})
 
-export const upload = multer({ storage });
+export const upload = multer({storage})
