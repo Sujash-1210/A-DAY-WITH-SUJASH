731625f98b4028a608fd8304efb9a11edde7ca51
Sujash-1210 <sujashdas80@gmail.com>
2025-07-07 13:08:31 +0530

    updated bug

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,12 @@ import userVideoRoutes from './routes/user/userVideoRoutes.js'
 import adminAuthRoutes from './routes/admin/adminAuthRoutes.js'
 import userContactRoutes from './routes/user/userContactRoutes.js'
 import path from 'path'
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+import { fileURLToPath } from 'url';
+
+// Get the current directory of the file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // Set up the uploads directory path
 const uploadsDir = path.join(__dirname, 'uploads');
